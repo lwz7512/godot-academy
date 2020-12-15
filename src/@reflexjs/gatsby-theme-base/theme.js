349308e222775a1203cb74d3d1be154f03a990c9
lwz7512 <lwz7512@gmail.com>
2020-12-14 21:36:06 -0400

    add courses page

--- src/@reflexjs/gatsby-theme-base/theme.js
+++ src/@reflexjs/gatsby-theme-base/theme.js
@@ -7,4 +7,23 @@ export default {
     primary: `#009ad6`,
     primaryHover: `#105176`
   },
+  styles: {
+    root: {
+      blockquote: {
+        padding: "16px",
+        backgroundColor: "#feeeed",
+        borderRadius: "4px",
+        borderLeft: t => `8px solid ${t.colors.primary}`,
+        // 👇
+        color: "#000",
+      },
+    },
+    hr: {
+      borderTop: "1px solid",
+      borderTopColor: "border",
+      marginTop: "4rem",
+      marginBottom: "6rem"
+    },
+    
+  }
 }
