a1a92dac1e738d7e2672d9d3f480ae26fba2aaa8
lwz7512 <lwz7512@gmail.com>
2021-01-04 23:25:20 -0400

    refactor slider component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,33 @@
 // https://github.com/erichbehrens/react-animated-slider
 import React from 'react'
 import Slider from 'react-animated-slider';
-import { Div, H3, Ul, Li, Container } from '@reflexjs/components';
+import { Div, H3, Ul, Li, P } from '@reflexjs/components';
+import { Image } from '@reflexjs/gatsby-plugin-image';
 
 
 export const SliderMDX = ({items}) => (
   <Slider >
     {items.map((s, i) => (
-      <Container key={i}>
-        <H3 textAlign="center" >{s.title}</H3>
-        <Div px="12" >{s.description}</Div>
-      </Container>
+      <Div key={i} >
+        {s.title &&
+          <H3 textAlign="center" >{s.title}</H3>
+        }
+        {s.banner &&
+            <Image src={s.banner} colStart="null|2" w="full" overflow="hidden" />
+        }
+        <Div px="14" position="absolute" top={s.title?24:8} left="0" >
+          <P fontSize="1.25rem" >{s.description && s.description}</P>
+          {s.list &&
+            <Ul ml="10" >
+              {
+                s.list.map((o, i) => (
+                  <Li key={i} p="1" fontSize="1.4rem" >{o}</Li>
+                ))
+              }
+            </Ul>
+          }
+        </Div>
+      </Div>
     ))}
   </Slider>
 )
