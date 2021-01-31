4d700cce38a4f9363d3c8c1c6a2c20e02df94f68
lwz7512 <lwz7512@gmail.com>
2021-01-31 19:43:58 -0400

    customize image component to reduce the homepage image size

---
+++ src/@reflexjs/gatsby-plugin-image/use-image.js
@@ -0,0 +1,58 @@
+import { useStaticQuery, graphql } from "gatsby"
+import { filterNodes } from "@reflexjs/utils"
+
+export const useImage = (filters) => {
+  filters = typeof filters === "string" && filters.replace(/^.?\//, "")
+
+  let data = useStaticQuery(graphql`
+    {
+      images: allFile(
+        filter: {
+          sourceInstanceName: { eq: "Image" }
+          extension: { ne: "svg" }
+        }
+      ) {
+        nodes {
+          src: relativePath
+          extension
+          publicURL
+          childImageSharp {
+            fluid(
+              cropFocus: CENTER
+              maxWidth: 800
+              quality: 80
+            ) {
+              base64
+              aspectRatio
+              src
+              srcSet
+              sizes
+            }
+          }
+        }
+      }
+      svgs: allFile(
+        filter: {
+          sourceInstanceName: { eq: "Image" }
+          extension: { eq: "svg" }
+        }
+      ) {
+        nodes {
+          src: relativePath
+          extension
+          publicURL
+        }
+      }
+    }
+  `)
+
+  return filterNodes(
+    {
+      allImages: {
+        nodes: [...data.images?.nodes, ...data.svgs?.nodes],
+      },
+    },
+    filters,
+    "src"
+  )
+}
